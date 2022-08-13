84fad3a6327c243a6956c4f0ec1a21d876807c7c
Josette Garcia <josettetgarcia@gmail.com>
2022-08-13 15:00:18 -0500

    created genre model, succeful route

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding data..."
+
+Genre.create(name: "Fantasy")
+Genre.create(name: "Romance")
+Genre.create(name: "Mystery")
+Genre.create(name: "Science Fiction")
+Genre.create(name: "Historical")
+Genre.create(name: "Memoir")
+Genre.create(name: "Self-Help")
+
+puts "Done seeding!"
