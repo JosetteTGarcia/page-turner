c7c65a24d533f7a5d22da9af342b45502df6700d
Josette Garcia <josettetgarcia@gmail.com>
2022-08-15 20:25:25 -0500

    setup signup, home, login, navbar components in client

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+
+import React, { useEffect, useState } from "react";
+// import { Switch, Route } from "react-router-dom";
+// import SignUp from "./SignUp";
+// import Login from "./Login";
+// import NavBar from "./NavBar";
+// import Home from "./Home";
+
+function App() {
+return (
+
+  <div> test</div>
+)
+}
+export default App;
