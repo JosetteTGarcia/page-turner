b286a054491bf531138eeac38d1f5a4bd4d0cc56
Josette Garcia <josettetgarcia@gmail.com>
2022-08-23 19:36:39 -0500

    fixed display issue on frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,49 @@
 
 import React, { useEffect, useState } from "react";
-// import { Switch, Route } from "react-router-dom";
-// import SignUp from "./SignUp";
-// import Login from "./Login";
-// import NavBar from "./NavBar";
-// import Home from "./Home";
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import Home from "./Home";
 
 function App() {
-return (
 
-  <div> test</div>
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+return (
+  <>
+    <NavBar user={user} setUser={setUser} />
+    <main>
+      {user ? (
+        <Routes>
+          <Route path="/" element={
+            <Home setUser={setUser} />}
+          />
+        </Routes>
+      ) : (
+        <Routes>
+           <Route path="/signup" element={
+            <SignUp setUser={setUser} />}
+          />
+         <Route path="/login" element={
+            <Login/>}
+          />
+          <Route path="/" element={
+            <Home/>}
+          />
+        </Routes>
+      )}
+    </main>
+   </>
 )
 }
 export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get 'genres', to: 'genres#index'
   get 'books', to: 'books#index'
 
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
 end
